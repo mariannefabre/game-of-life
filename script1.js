897bc225017e09bb71f106bf3c54e4c69e83e812
mariannefabre <marianne.fabre@hotmail.fr>
2021-04-23 11:07:18 +0200

    change to pure function

---
+++ script1.js
@@ -0,0 +1,130 @@
+function create2DArray(cols, rows) {
+  let arr = new Array(cols);
+  for (let i = 0; i < cols; i++) {
+    arr[i] = new Array(rows);
+  }
+  return arr;
+}
+
+// Initialize grid with random structures
+function initRandomGrid(cols, rows) {
+  let grid = create2DArray(cols, rows);
+  for (let i = 0; i < cols; i++) {
+    for (let j = 0; j < rows; j++) {
+      grid[i][j] = Math.floor(Math.random() * 2);
+    }
+  }
+  return grid;
+}
+
+function drawGrid(grid, cols, rows, ctx, cellSize) {
+  for (let i = 0; i < cols; i++) {
+    for (let j = 0; j < rows; j++) {
+      drawCell(grid, i, j, ctx, cellSize);
+    }
+  }
+}
+
+function drawCell(grid, col, row, ctx, cellSize) {
+  const aliveColor = "cornflowerblue";
+  const deadColor = "white";
+  let color;
+  if (!grid) {
+    color = deadColor;
+  } else {
+    color = grid[col][row] === 0 ? deadColor : aliveColor;
+  }
+  ctx.fillStyle = color;
+  ctx.fillRect(row * cellSize, col * cellSize, cellSize, cellSize);
+  ctx.strokeStyle = "lightgray";
+  ctx.strokeRect(row * cellSize, col * cellSize, cellSize, cellSize);
+}
+
+function getNextGen(grid, cols, rows) {
+  let nextGrid = create2DArray(cols, rows);
+  for (let i = 0; i < cols; i++) {
+    for (let j = 0; j < rows; j++) {
+      let neighbors = countNeighbors(grid, i, j, cols, rows);
+      if ((grid[i][j] === 1 && neighbors === 2) || neighbors === 3) {
+        nextGrid[i][j] = 1;
+      } else {
+        nextGrid[i][j] = 0;
+      }
+    }
+  }
+  return nextGrid;
+}
+
+function countNeighbors(grid, col, row, cols, rows) {
+  let count = 0;
+  if (col - 1 >= 0) {
+    count += grid[col - 1][row];
+  }
+  if (col - 1 >= 0 && row - 1 >= 0) {
+    count += grid[col - 1][row - 1];
+  }
+  if (col - 1 >= 0 && row + 1 < rows) {
+    count += grid[col - 1][row + 1];
+  }
+  if (row - 1 >= 0) {
+    count += grid[col][row - 1];
+  }
+  if (row + 1 < rows) {
+    count += grid[col][row + 1];
+  }
+  if (col + 1 < cols) {
+    count += grid[col + 1][row];
+  }
+  if (col + 1 < cols && row - 1 >= 0) {
+    count += grid[col + 1][row - 1];
+  }
+  if (col + 1 < cols && row + 1 < rows) {
+    count += grid[col + 1][row + 1];
+  }
+  return count;
+}
+
+function updateGrid(grid, cols, rows, ctx, cellSize) {
+  grid = getNextGen(grid, cols, rows);
+  drawGrid(grid, cols, rows, ctx, cellSize);
+}
+
+function init() {
+  const canvas = document.getElementById("canvas");
+  const ctx = canvas.getContext("2d");
+  const cellSize = 100;
+  const cols = Math.floor(canvas.width / cellSize);
+  const rows = Math.floor(canvas.height / cellSize);
+  let grid = null;
+  let interval;
+  const startBtn = document.getElementById("start");
+  startBtn.addEventListener("click", start);
+  const pauseBtn = document.getElementById("pause");
+  pauseBtn.addEventListener("click", pause);
+  const resetBtn = document.getElementById("reset");
+  resetBtn.addEventListener("click", reset);
+
+  function start() {
+    if (!grid) {
+      grid = initRandomGrid(cols, rows);
+      // passing grid as an argument will only pass its value, not its reference!
+      drawGrid(grid, cols, rows, ctx, cellSize);
+    }
+    interval = window.setInterval(() => {
+      updateGrid(grid, cols, rows, ctx, cellSize);
+    }, 2000);
+    startBtn.removeEventListener("click", start);
+  }
+  function pause() {
+    clearInterval(interval);
+    startBtn.addEventListener("click", start);
+  }
+  function reset() {
+    pause();
+    grid = null;
+    initRandomGrid(cols, rows);
+    drawGrid(grid, cols, rows, ctx, cellSize);
+  }
+}
+
+init();
